661e795ffb3d2c0efd37c9015f297024b14f3a95
Maxi Burastero <maxicc4@gmail.com>
2020-05-17 13:31:01 -0300

    Se acomodan algunas cosas en el home. Se centran algunos elementos. Se pruebas estilos nuevos.

---
+++ js/home.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.circle').hover(
+        function () {
+            $(this).css('padding-top', '48px');
+            $(this).css('background-color', 'rgb(184,170,170)');
+            $(this).css('color', '#ffffff');
+            $(this).css('font-weight', 'bold');
+            $(this).find('.view-more').show();
+            $(this).find('.tip-content').hide();
+    },
+        function () {
+            $(this).find('.view-more').hide();
+            $(this).find('.tip-content').show();
+            $(this).removeAttr('style');
+        })
+
+});
