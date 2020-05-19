d4330c41f8c259f7027335d56bfcc17dc4f5dc5b
Maxi Burastero <maxicc4@gmail.com>
2020-05-19 18:35:26 -0300

    Home: se agrega modal para ampliar info de los tips. Se lo muestra y se lo oculta.

--- js/home.js
+++ js/home.js
@@ -5,12 +5,17 @@ $(document).ready(function () {
             $(this).css('color', '#ffffff');
             $(this).css('font-weight', 'bold');
             $(this).find('.view-more').show();
-            $(this).find('.tip-content').hide();
-    },
+            $(this).find('.tip-title').hide();
+        },
         function () {
             $(this).find('.view-more').hide();
-            $(this).find('.tip-content').show();
+            $(this).find('.tip-title').show();
             $(this).removeAttr('style');
-        })
+    });
+
+    $('.circle').click(function () {
+        $('#tipsModal').modal('show');
+        $('#tipsWindow').html($(this).find('.tip-title').html());
+    })
 
 });
