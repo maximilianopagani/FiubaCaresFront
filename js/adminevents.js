18f0075f2205a852d0067e4c631ef8c316486dba
florencia95 <florenciaerramuspe@hotmail.com>
2020-06-14 20:53:43 -0300

    Borrado de eventos

--- js/adminevents.js
+++ js/adminevents.js
@@ -1,30 +1,3 @@
-$(document).ready(function () {
-    let container = $("#container");
-    $.ajax({
-        type: "GET",
-        url: "https://fiuba-cares-back.herokuapp.com/api/events/",
-        contentType: "application/json; charset=UTF-8;",
-        headers: {Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjN9.oF_jJKavmWrM6d_io5M5PBiK9AKMf_OcK4xpc17kvwI"},
-        dataType: "json",
-        success: function (data) {
-            console.log(data);
-            data.forEach(function (element) {
-                container.append(getEventCardHTML(element))
-            });
-            $('.btn-delete').click(function () {
-                alert("se va a borrar el elemento");
-            });
-            $('.btn-modify').click(function () {
-                location.href = "./modevent.html?event_id=" + $(this).data('id');
-            });
-
-        }
-    });
-    $('.btn-addelement').click(function () {
-        location.href = './addevent.html';
-    });
-});
-
 function getEventCardHTML(element) {
     return '<div class="row">\n' +
         '        <div class="col-12 mt-3">\n' +
@@ -43,3 +16,63 @@ function getEventCardHTML(element) {
         '        </div>\n' +
         '    </div>';
 }
+
+function getURLParam(param){
+  return new URLSearchParams(window.location.search).get(param);
+}
+
+function deleteEvent(btn) {
+    const id_event = $(btn).data("id");
+    console.log(id_event);
+    alert("Se va a borrar el evento");
+    let settings = {
+        url: "https://fiuba-cares-back.herokuapp.com/api/events/" + id_event,
+        type: "DELETE",
+        contentType: "application/json",
+        dataType: "json",
+        headers: {
+            Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjN9.oF_jJKavmWrM6d_io5M5PBiK9AKMf_OcK4xpc17kvwI"
+        },
+        success: function (data){
+          console.log("se borro el evento");  
+        }
+    };
+ 
+    $.ajax(settings)
+        .done(function() {
+            alert("Se borró el evento correctamente");
+        })
+        .fail(function() {
+            alert("No se borró el evento");
+        })
+        .always(function() {
+            setTimeout("location.href = './adminevents.html';", 2500);
+        });
+}
+
+$(document).ready(function () {
+    let container = $("#container");
+    $.ajax({
+        type: "GET",
+        url: "https://fiuba-cares-back.herokuapp.com/api/events/",
+        contentType: "application/json; charset=UTF-8;",
+        headers: {Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjN9.oF_jJKavmWrM6d_io5M5PBiK9AKMf_OcK4xpc17kvwI"},
+        dataType: "json",
+        success: function (data) {
+            data.forEach(function (element) {
+                container.append(getEventCardHTML(element))
+            });
+            $('.btn-delete').click(function () {
+                deleteEvent($(this));
+            });
+            $('.btn-modify').click(function () {
+                location.href = "./modevent.html?event_id=" + $(this).data('id');
+            });
+
+        }
+    });
+    $('.btn-addelement').click(function () {
+        location.href = './addevent.html';
+    });
+});
+
