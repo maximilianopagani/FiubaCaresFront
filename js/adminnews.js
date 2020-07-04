297fb5ff317897f3185665473628ca78cc6225ac
florencia95 <florenciaerramuspe@hotmail.com>
2020-07-04 19:37:24 -0300

    Agrego vista del newsletter para admin y boton de crear noticia

---
+++ js/adminnews.js
@@ -0,0 +1,34 @@
+function getNewsCardHTML(element) {
+    return '<div class="row">\n' +
+        '        <div class="col-12 mt-3">\n' +
+        '            <div class="card">\n' +
+        '                <div class="card-horizontal">\n' +
+        '                    <div class="card-body">\n' +
+        '                       <div class="card-title">\n' + element.title + '\n' +
+         '                    </div>\n' +
+        '                </div>\n' +
+        '\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </div>';
+}
+
+
+$(document).ready(function () {
+    let container = $("#container");
+    $.ajax({
+        type: "GET",
+        url: "https://fiuba-cares-back.herokuapp.com/api/newsletter/",
+        contentType: "application/json; charset=UTF-8;",
+        headers: {Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjN9.oF_jJKavmWrM6d_io5M5PBiK9AKMf_OcK4xpc17kvwI"},
+        dataType: "json",
+        success: function (data) {
+            data.forEach(function (element) {
+                container.append(getNewsCardHTML(element))
+            });
+        }
+    });
+    $('.btn-addelement').click(function () {
+        location.href = './addnews.html';
+    });
+});
