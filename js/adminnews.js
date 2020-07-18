68eb4d5fa0027be3e8c0b52253da5139cde49e85
maximilianopagani <maxipagani@hotmail.com>
2020-07-18 13:06:24 -0300

    Delete newsletter entry

--- js/adminnews.js
+++ js/adminnews.js
@@ -5,7 +5,10 @@ function getNewsCardHTML(element) {
         '                <div class="card-horizontal">\n' +
         '                    <div class="card-body">\n' +
         '                       <div class="card-title">\n' + element.title + '\n' +
-         '                    </div>\n' +
+        '                       <div style="display: inline">\n' +
+        '                           <a href="#" class="btn btn-primary-custom btn-delete float-right" data-id="' + element._id + '">Borrar</a>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
         '                </div>\n' +
         '\n' +
         '            </div>\n' +
@@ -13,6 +16,31 @@ function getNewsCardHTML(element) {
         '    </div>';
 }
 
+function deleteEntry(btn) {
+    const id_entry = $(btn).data("id");
+    console.log(id_entry);
+    alert("La noticia seleccionada será borrada");
+    let settings = {
+        url: "https://fiuba-cares-back.herokuapp.com/api/newsletter/" + id_entry,
+        type: "DELETE",
+        contentType: "application/json",
+        dataType: "json",
+        headers: {
+            Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjN9.oF_jJKavmWrM6d_io5M5PBiK9AKMf_OcK4xpc17kvwI"
+        }
+    };
+
+    $.ajax(settings)
+        .done(function() {
+            alert("Se borró la noticia correctamente");
+        })
+        .fail(function() {
+            alert("Hubo un error y no se pudo borrar la noticia. Intente mas tarde.");
+        })
+        .always(function() {
+            setTimeout("location.href = './adminnews.html';", 2500);
+        });
+}
 
 $(document).ready(function () {
     let container = $("#container");
@@ -25,6 +53,9 @@ $(document).ready(function () {
         success: function (data) {
             data.forEach(function (element) {
                 container.append(getNewsCardHTML(element))
+            });
+            $('.btn-delete').click(function () {
+                deleteEntry($(this));
             });
         }
     });
